5d485424513c13288758ad4b00a35af35f334b63
Deep Patel <pateldeep682@gmail.com>
2023-12-03 13:39:32 +0530

    Added redis sync part.

--- setup.sh
+++ setup.sh
@@ -1,3 +1,4 @@
 #! /bin/sh
 
+mkdir -p data
 sudo chown -R 1001:1001 data
--- sync_mongo/mongo_publisher.js
+++ sync_mongo/mongo_publisher.js
@@ -1,16 +1,38 @@
 import dotenv from 'dotenv';
 import { Kafka } from 'kafkajs';
 import mongoose from 'mongoose';
+import redis from 'redis';
 import Stock from './models/stock.js';
 
-dotenv.config();
+if (process.env.NODE_ENV !== 'production') {
+    dotenv.config();
+}
 
 // connect to mongodb & listen for requests
 const dbURI = process.env.DB_URI;
-
-console.log(`DB URI: ${dbURI}`);
-
 mongoose.connect(dbURI)
+
+const redis_connection_config = {
+    host: process.env.REDIS_HOST,
+    port: process.env.REDIS_PORT,
+}
+const redisClient = redis.createClient(
+    {
+        socket: redis_connection_config,
+        password: process.env.REDIS_PASS,
+        retry_strategy: () => 1000
+    },
+);
+
+await redisClient.connect();
+
+redisClient.on('connect', () => {
+    console.log('Connected to Redis');
+});
+
+redisClient.on('error', (err) => {
+    console.error('Error:', err);
+});
 
 const kafka = new Kafka({
     clientId: 'kafka-stock',
@@ -34,15 +56,20 @@ await consumer.run({
         console.log(`Message from kafka: ${message.value}`)
         const message_json = JSON.parse(message.value.toString())
         const message_data = message_json['data']
-        message_data.forEach(element => {
+        message_data.forEach(async element => {
             const stock_data = {
                 symbol: element['s'],
                 price: element['p'],
                 time: element['t'],
                 volume: element['v']
             }
+            redisClient.publish(element['s'], JSON.stringify(stock_data)).then(result => {
+                console.log(result);
+            }).catch(err => {
+                console.log(err);
+            });
             const stock = new Stock(stock_data);
-            stock.save()
+            await stock.save()
                 .then(result => {
                     console.log(result);
                 })
